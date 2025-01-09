7b43682240c2cb5337022a9fa032a696233f9ab5
zhengke.gzk <zhengke.gzk@antgroup.com>
2025-01-09 16:25:57 +0800

    comment report_pipeline

--- kag/solver/implementation/table/table_reasoner.py
+++ kag/solver/implementation/table/table_reasoner.py
@@ -194,7 +194,7 @@ class TableReasoner(KagReasonerABC):
                 self.resp_think_generator,
                 with_except=True,
             )
-        self.report_pipleline(history, final_answer, final_answer_form_llm)
+        #self.report_pipleline(history, final_answer, final_answer_form_llm)
         return final_answer
 
     def _get_sub_question_list(self, history: SearchTree, kg_content: str):
